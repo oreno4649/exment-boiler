5d8884c03b10cb8dd58b5fa476f7bd017f7389bd
Nobuyuki Tabuchi <oreno4649@gmail.com>
2022-07-23 17:46:22 +0900

    feat laravel 9

--- public/vendor/exment/js/publicform.getbox.js
+++
@@ -1,9 +0,0 @@
-var Exment;
-(function (Exment) {
-    class GetBoxPublicForm extends Exment.GetBox {
-        getBox() {
-            return $('.form-horizontal');
-        }
-    }
-    Exment.GetBoxPublicForm = GetBoxPublicForm;
-})(Exment || (Exment = {}));
